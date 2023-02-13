714b94903b3fd495d8155fcb82b0995ad5ca1bb9
Josh Tucker <joshuaatucker@hotmail.com>
2023-02-13 22:27:20 +0000

    nav bar scss updated

--- src/Components/Checkbox/Checkbox.jsx
+++ src/Components/Checkbox/Checkbox.jsx
@@ -5,39 +5,41 @@ const Checkbox = ({ handleCheck }) => {
   return (
     <div className="checkbox">
       <h3 className="checkbox__title">Or rather a selection?</h3>
-      <label htmlFor="checkBoxOne" className="checkbox__one">
-        High Abv 
-      </label>
-      <input
-        onChange={handleCheck}
-        className="checkbox__one"
-        type="checkbox"
-        id="checkBoxOne"
-        name="checkBoxOne"
-        value="abv"
-      ></input>
-      <label htmlFor="checkBoxTwo" className="checkbox__two">
-        Classic Range
-      </label>
-      <input
-        onChange={handleCheck}
-        className="checkbox__two"
-        type="checkbox"
-        id="checkBoxTwo"
-        name="checkBoxTwo"
-        value="classic"
-      ></input>
-      <label htmlFor="checkBoxThree" className="checkbox__three">
-        PH > 4%
-      </label>
-      <input
-        onChange={handleCheck}
-        className="checkbox__three"
-        type="checkbox"
-        id="checkBoxThree"
-        name="checkBoxThree"
-        value="ph"
-      ></input>
+      <div className="checkbox__container">
+        <label htmlFor="checkBoxOne" className="checkbox__one-label">
+          High Abv
+        </label>
+        <input
+          onChange={handleCheck}
+          className="checkbox__one"
+          type="checkbox"
+          id="checkBoxOne"
+          name="checkBoxOne"
+          value="abv"
+        ></input>
+        <label htmlFor="checkBoxTwo" className="checkbox__two-label">
+          Classic Range
+        </label>
+        <input
+          onChange={handleCheck}
+          className="checkbox__two"
+          type="checkbox"
+          id="checkBoxTwo"
+          name="checkBoxTwo"
+          value="classic"
+        ></input>
+        <label htmlFor="checkBoxThree" className="checkbox__three-label">
+          PH {`>`} 4%
+        </label>
+        <input
+          onChange={handleCheck}
+          className="checkbox__three"
+          type="checkbox"
+          id="checkBoxThree"
+          name="checkBoxThree"
+          value="ph"
+        ></input>
+      </div>
     </div>
   );
 };
--- src/Containers/Nav/Nav.jsx
+++ src/Containers/Nav/Nav.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import Search from "../../Components/Search/Search";
 import Checkbox from "../../Components/Checkbox/Checkbox";
 import "./Nav.scss";
+import HomeMenu from "../../assets/images/home.png";
+import ProfileMenu from "../../assets/images/menu.png";
 
 
 const Nav = ({handleSearch, handleCheck}) => {
  
 
       return (
-        <div className="nav-container">
-          <Search handleSearch={handleSearch}/>
-          <Checkbox handleCheck={handleCheck}/>
+        <div className="nav">
+          <img src={HomeMenu} alt="home-menu"/>
+          <div className="nav__content-container">
+            <h1 className="nav__countdown">Countdown timer</h1>
+          <Search className="nav__search" handleSearch={handleSearch}/>
+          <Checkbox className="nav__check" handleCheck={handleCheck}/>
+
+          </div>
+          <img src={ProfileMenu} alt="profile-menu"/>
 
         </div>
       );
